610448da2c17d8a6e8faedf1a5faee9090cc9755
Ok4mii <chrishopkins512@gmail.com>
2022-06-16 16:35:12 -0500

    Initial
    Initial Commit

---
+++ Assets/MenuControl.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class MenuControl : MonoBehaviour
+{
+    [Header("Volume Settings")]
+    [SerializeField] private Slider volumeSlider = null;
+    [SerializeField] private float defaultVolume = 0.5f;
+
+    [Header("Confirmation Prompt")]
+    [SerializeField] private GameObject confirmationPrompt = null;
+
+    [Header("Levels to Load")]
+    public string _newLevel;
+    private string _levelToLoad;
+    [SerializeField] private GameObject noSavedGameDialogue = null;
+
+    public void NewGameDialogueYes()
+    {
+        SceneManager.LoadScene(_newLevel);
+    }
+
+    public void LoadGameDialogueYes()
+    {
+        if(PlayerPrefs.HasKey("SavedLevel"))
+        {
+            _levelToLoad = PlayerPrefs.GetString("SavedLevel");
+            SceneManager.LoadScene(_levelToLoad);
+        }
+
+        else
+        {
+            noSavedGameDialogue.SetActive(true);
+        }
+    }
+
+    public void Exitbutton()
+    {
+        Application.Quit();
+    }
+
+    public void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+    }
+
+    public void VolumeApply()
+    {
+        PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
+        StartCoroutine(ConfirmationBox());
+    }
+
+    public void ResetButton(string OptionType)
+    {
+        if (OptionType == "Audio")
+        {
+            AudioListener.volume = defaultVolume;
+            volumeSlider.value = defaultVolume;
+            VolumeApply();
+        }
+    }
+
+    public IEnumerator ConfirmationBox()
+    {
+        confirmationPrompt.SetActive(true);
+        yield return new WaitForSeconds(2);
+        confirmationPrompt.SetActive(false);
+    }
+    
+}
